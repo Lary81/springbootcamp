400c9b5e8d855486e288a8ae221b19685c5970dd
Radosław Szmit <radoslawszmit@gmail.com>
2017-07-03 16:40:23 +0200

    adnotacje @Component

--- src/main/java/pl/com/sages/spring/bootcamp/service/ProductService.java
+++ src/main/java/pl/com/sages/spring/bootcamp/service/ProductService.java
@@ -1,11 +1,11 @@
 package pl.com.sages.spring.bootcamp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import pl.com.sages.spring.bootcamp.dao.ProductDao;
 import pl.com.sages.spring.bootcamp.model.Product;
 
-@Component
+@Service
 public class ProductService {
 
     @Autowired
